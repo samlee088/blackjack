71065eafc66d78fb32100668eba1e6adc31950b8
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-07-29 19:06:15 -0700

    playRound

--- main.py
+++ main.py
@@ -1,188 +1,189 @@
 from enum import Enum
-import random
-from abc import ABC, abstractmethod
-
 
 class Suit(Enum):
     CLUBS, DIAMONDS, HEARTS, SPADES = 'clubs', 'diamonds', 'hearts', 'spades'
 
-class Card():
+class Card:
     def __init__(self, suit, value):
-        self.suit = suit
-        self.value = value
+        self._suit = suit
+        self._value = value
     
     def getSuit(self):
-        return self.suit
-    
+        return self._suit
+
     def getValue(self):
-        return self.value
-    
-    def printCard(self):
+        return self._value
+
+    def print(self):
         print(self.getSuit(), self.getValue())
 
-class Hand():
+
+import random
+
+class Deck:
     def __init__(self):
-        self.score = 0
-        self.cards = []
+        self._cards = []
+        for suit in Suit:
+            for value in range(1, 14):
+                self._cards.append(Card(suit, min(value, 10)))
+
+    def print(self):
+        for card in self._cards:
+            card.print()
+
+    def draw(self):
+        return self._cards.pop()
+
+    def shuffle(self):
+        for i in range(len(self._cards)):
+            j = random.randint(0, 51)
+            self._cards[i], self._cards[j] = self._cards[j], self._cards[i]
+
+class Hand:
+    def __init__(self):
+        self._score = 0
+        self._cards = []
 
     def addCard(self, card):
-        self.cards.append(card)
+        # Note: there is a subtle bug here. 
+        # If an ace is in the hand, which counts as an 11,
+        # and the player draws another card, which makes the score > 21.
+        self._cards.append(card)
         if card.getValue() == 1:
-            self.score += 11 if self.score <= 21 else 1
-        else :
-            self.score += card.getValue()
-        print('Score:', self.score)
-    
+            self._score += 11 if self._score + 11 <= 21 else 1
+        else:
+            self._score += card.getValue()
+        print('Score: ', self._score)
+
     def getScore(self):
-        return self.score
-    
+        return self._score
+
     def getCards(self):
-        return self.cards
-    
+        return self._cards
+
     def print(self):
         for card in self.getCards():
             print(card.getSuit(), card.getValue())
 
+from abc import ABC, abstractmethod
 
-print, draw, shuffle
-
-class Deck:
-    
-    def __init__(self):
-        self.deck = []
-        for suit in Suit:
-            for value in range(1, 14):
-                self.cards.append(Card(suit, min(value, 10)))
-
-    def print(self):
-        for card in self.cards:
-            card.print()
-    
-    def draw(self):
-        return self.cards.pop()
-    
-    def shuffle(self):
-        for i in range(len(self.cards)):
-            j = random.randint(0, 51)
-            self.cards[i], self.cards[j] = self.cards[j], self.cards[i]
-
-
-class Player:
+class Player(ABC):
     def __init__(self, hand):
-        self.hand = hand
+        self._hand = hand
 
     def getHand(self):
-        return self.hand
-    
+        return self._hand
+
     def clearHand(self):
-        self.hand = Hand()
+        self._hand = Hand()
 
     def addCard(self, card):
-        self.hand.addCard(card)
+        self._hand.addCard(card)
 
     @abstractmethod
     def makeMove(self):
         pass
 
-
 class UserPlayer(Player):
     def __init__(self, balance, hand):
         super().__init__(hand)
-        self.balance = balance
+        self._balance = balance
 
     def getBalance(self):
-        return self.balance
-    
+        return self._balance
+
     def placeBet(self, amount):
-        if amount > self.balance:
-            raise ValueError("Insufficient Funds")
-        self.balance -= amount
+        if amount > self._balance:
+            raise ValueError('Insufficient funds')
+        self._balance -= amount
         return amount
-    
+
     def receiveWinnings(self, amount):
-        self.balance += amount
+        self._balance += amount
 
     def makeMove(self):
-        if self.getHand().getScore()>21:
+        if self.getHand().getScore() > 21:
             return False
-        move = input("Draw card? [y/n]")
+        move = input('Draw card? [y/n] ')
         return move == 'y'
-    
+
 class Dealer(Player):
     def __init__(self, hand):
         super().__init__(hand)
-        self.targetScore = 17
+        self._targetScore = 17
 
     def updateTargetScore(self, score):
-        self.targetScore = score
-    
+        self._targetScore = score
+
     def makeMove(self):
-        return self.getHand().getScore() < self.targetScore
+        return self.getHand().getScore() < self._targetScore
 
 class GameRound:
     def __init__(self, player, dealer, deck):
-        self.player = player
-        self.dealer = dealer
-        self.deck = deck
-    
+        self._player = player
+        self._dealer = dealer
+        self._deck = deck
+
     def getBetUser(self):
-        amount = int(input("Enter a bet amount: "))
+        amount = int(input('Enter a bet amount: '))
         return amount
-    
+
     def dealInitialCards(self):
         for i in range(2):
-            self.player.addCard(self.deck.draw())
-            self.player.addCard(self.deck.draw())
+            self._player.addCard(self._deck.draw())
+            self._dealer.addCard(self._deck.draw())
         print('Player hand: ')
         self._player.getHand().print()
         dealerCard = self._dealer.getHand().getCards()[0]
         print("Dealer's first card: ")
         dealerCard.print()
-    
+
     def cleanupRound(self):
-        self.player.clearHand()
-        self.dealer.clearHand()
-        print("Player balance: ", self.player.getBalance())
+        self._player.clearHand()
+        self._dealer.clearHand()
+        print('Player balance: ', self._player.getBalance())
 
     def play(self):
-        self.deck.shuffle()
+        self._deck.shuffle()
 
-        if self.player.getBalance() <= 0:
+        if self._player.getBalance() <= 0:
             print('Player has no more money =)')
             return
         userBet = self.getBetUser()
-        self.player.placeBet(userBet)
+        self._player.placeBet(userBet)
 
         self.dealInitialCards()
 
         # User makes moves
-        while self.player.makeMove():
-            drawnCard = self.deck.draw()
+        while self._player.makeMove():
+            drawnCard = self._deck.draw()
             print('Player draws', drawnCard.getSuit(), drawnCard.getValue())
-            self.player.addCard(drawnCard)
-            print('Player score: ', self.player.getHand().getScore())
+            self._player.addCard(drawnCard)
+            print('Player score: ', self._player.getHand().getScore())
 
-        if self.player.getHand().getScore() > 21:
-            print("Player busts!")
+        if self._player.getHand().getScore() > 21:
+            print('Player busts!')
             self.cleanupRound()
             return
         
         # Dealer makes moves
-        while self.dealer.makeMove():
-            self.dealer.addCard(self.deck.draw())
-
+        while self._dealer.makeMove():
+            self._dealer.addCard(self._deck.draw())
+        
         # Determine winner
-        if self.dealer.getHand().getScore() > 21 or self.player.getHand().getScore() > self.dealer.getHand().getScore():
-            print('Player Wins!')
-            self.player.receiveWinnings(userBet * 2)
-        elif self.dealer.getHand().getScore() > self.player.getHand().getScore():
+        if self._dealer.getHand().getScore() > 21 or self._player.getHand().getScore() > self._dealer.getHand().getScore():
+            print('Player wins')
+            self._player.receiveWinnings(userBet * 2)
+        elif self._dealer.getHand().getScore() > self._player.getHand().getScore():
             print('Player loses')
         else:
-            print("Game ends in a draw")
-            self.player.receiveWinnings(userBet)
+            print('Game ends in a draw')
+            self._player.receiveWinnings(userBet)
         self.cleanupRound()
 
-player = UserPlayer(10000, Hand())
+
+player = UserPlayer(1000, Hand())
 dealer = Dealer(Hand())
-while player.getBalance > 0:
+
+while player.getBalance() > 0:
     gameRound = GameRound(player, dealer, Deck()).play()
